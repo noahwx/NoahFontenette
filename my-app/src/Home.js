67df2a15a33ab6622424361bcc3ef26327aec265
noahwx <noahfontenette@outlook.com>
2023-03-16 19:47:12 -0500

    0.0.507

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import logo from './logo.svg';
 import './index.css';
@@ -23,24 +23,41 @@ const Home = () => {
         setOpen(false);
     };
 
+    const [theme, setTheme] = useState('light');
+    const toggleTheme = () => {
+      if (theme === 'light') {
+          setTheme('dark');
+      } else {
+          setTheme('light');
+      }
+    };
+
+    useEffect(() => {
+      localStorage.setItem('theme', theme);
+      document.body.className = theme;
+    }, [theme]);
+
     return (
         <div>
 
             <div className="mobile-wrapper">
-                <div className='mobile-menu'>
-                    <div className='menu-btn'>
-                        <img src={logo} alt='' onClick={handleOpen} className='mobile-btn'/>
-                    </div>
-
-                    {open ? (
-                        <div className='mobile-Nav'>
-                            <div className='mobile-NavItems'>
-                                <NavLink to='/' className='mobile-NavItem' onClick={handleMenuOne}>Home</NavLink>
-                                <NavLink to='/Projects' className='mobile-NavItem' onClick={handleMenuTwo}>Projects</NavLink>
-                                <NavLink to='/Contact' className='mobile-NavItem' onClick={handleMenuThree}>Contact</NavLink>
-                            </div>
+                <div className={`App ${theme}`}>
+                    <div className='mobile-menu'>
+                        <div className='menu-btn'>
+                            <img src={logo} alt='' onClick={handleOpen} className='mobile-btn'/>
                         </div>
-                    ) : null}
+
+                        {open ? (
+                            <div className='mobile-Nav'>
+                                <div className='mobile-NavItems'>
+                                    <NavLink to='/' className='mobile-NavItem' onClick={handleMenuOne}>Home</NavLink>
+                                    <NavLink to='/Projects' className='mobile-NavItem' onClick={handleMenuTwo}>Projects</NavLink>
+                                    <NavLink to='/Contact' className='mobile-NavItem' onClick={handleMenuThree}>Contact</NavLink>
+                                    <NavLink onClick={toggleTheme} className='mobile-NavItem'>Toggle Theme</NavLink>
+                                </div>
+                            </div>
+                        ) : null}
+                    </div>
                 </div>
             </div>
         </div>
